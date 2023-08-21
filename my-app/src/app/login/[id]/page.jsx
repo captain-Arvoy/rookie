50993f98d5de84d00d6a116e6a9ebf2b96b76adf
captain-arvoy <accofstardust@gmail.com>
2023-08-21 12:19:17 +0530

    corrected gitignore  dyanamic routing

--- my-app/src/app/login/[id]/page.jsx
+++ my-app/src/app/login/[id]/page.jsx
@@ -1,5 +1,10 @@
 export default function page({ params }){
   return(
-    <h1> Argument = {params.id} </h1>
+    <div className="bg-slate-300 text-xl p-8 m-4 text-slate-900"> 
+      Hello👋,
+      <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+        <span class="relative text-white ">{params.id}</span>
+      </span>
+      </div>
   )
 }
